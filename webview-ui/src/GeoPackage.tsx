8c4c5b1c3c4c7fb53abca7052b99523872bd5e1a
PZahnen <124141114+PZahnen@users.noreply.github.com>
2023-10-12 15:05:36 +0200

    checkboxes for gpkg tables

--- webview-ui/src/GeoPackage.tsx
+++ webview-ui/src/GeoPackage.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import { VSCodeCheckbox } from "@vscode/webview-ui-toolkit/react";
+
+import getGeoPackageTables from "./Testdaten/GeoPackageTabellen";
 
 function GeoPackage(selectedDataSource: any) {
   const [GPKG, setGPKG] = useState<File | null | Blob>(null);
   const [filename, setFilename] = useState<string>("");
+  const geoPackageTables: string[] = Object.keys(getGeoPackageTables());
+  const [selectedGeoPackageTable, setSelectedGeoPackageTable] = useState<string[]>([]);
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -21,30 +26,58 @@ function GeoPackage(selectedDataSource: any) {
     }
   };
 
+  const handleTableSelection = (tableName: string) => {
+    if (selectedGeoPackageTable.includes(tableName)) {
+      setSelectedGeoPackageTable(selectedGeoPackageTable.filter((table) => table !== tableName));
+    } else {
+      setSelectedGeoPackageTable([...selectedGeoPackageTable, tableName]);
+    }
+  };
+
   useEffect(() => {
     if (selectedDataSource !== "GeoPackage") {
       setGPKG(null);
       setFilename("");
+      setSelectedGeoPackageTable([]);
     }
   }, [selectedDataSource]);
 
-  console.log("filename", filename);
+  console.log(selectedGeoPackageTable);
 
   return (
-    <div className="button-container">
-      <label htmlFor="geoInput" id="uploadLabel">
-        Choose File
-      </label>
-      <input
-        id="geoInput"
-        type="file"
-        onChange={(event) => onFileChange(event)}
-        accept=".gpkg"
-        multiple={false}
-        style={{ display: "none" }}
-      />
-      {filename !== "" && <span id="GpkgName">{filename}</span>}
-    </div>
+    <>
+      <div className="button-container">
+        <label htmlFor="geoInput" id="uploadLabel">
+          Choose File
+        </label>
+        <input
+          id="geoInput"
+          type="file"
+          onChange={(event) => onFileChange(event)}
+          accept=".gpkg"
+          multiple={false}
+          style={{ display: "none" }}
+        />
+        {filename !== "" && <span id="GpkgName">{filename}</span>}
+      </div>
+      {filename !== "" && (
+        <form>
+          <fieldset>
+            <legend>Choose tables</legend>
+            <div className="checkbox-container">
+              {geoPackageTables.map((tableName) => (
+                <VSCodeCheckbox
+                  key={tableName}
+                  checked={selectedGeoPackageTable.includes(tableName)}
+                  onChange={() => handleTableSelection(tableName)}>
+                  {tableName}
+                </VSCodeCheckbox>
+              ))}
+            </div>
+          </fieldset>
+        </form>
+      )}
+    </>
   );
 }
 
---
+++ webview-ui/src/Testdaten/GeoPackageTabellen.ts
@@ -0,0 +1,14 @@
+function getGeoPackageTables() {
+  const GeoPackageTables: any = {
+    table01: 12,
+    table02: 13,
+    table03: 14,
+    table04: 15,
+    table05: 16,
+    table06: 17,
+    table07: 18,
+  };
+  return GeoPackageTables;
+}
+
+export default getGeoPackageTables;
