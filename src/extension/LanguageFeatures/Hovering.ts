9f3f26f302ef744cf5db7a4aaeb06533583bdd74
PZahnen <124141114+PZahnen@users.noreply.github.com>
2023-11-14 17:50:48 +0100

    introducing hovertexts

---
+++ src/extension/LanguageFeatures/Hovering.ts
@@ -0,0 +1,70 @@
+import * as vscode from "vscode";
+import { hoverData } from "./providers";
+
+interface HoverTexts {
+  [key: string]: string;
+}
+
+interface LooseDefinition {
+  title?: string;
+  description?: string;
+  [key: string]: any;
+}
+
+interface DefinitionsMap {
+  [key: string]: LooseDefinition;
+}
+
+function processProperties(
+  definitions: Record<string, LooseDefinition>,
+  definitionsMap: DefinitionsMap = {}
+) {
+  for (const key in definitions) {
+    const definition = definitions[key];
+    if (definition.properties) {
+      for (const propKey in definition.properties) {
+        const propDefinition = definition.properties[propKey];
+        if (propDefinition.title || propDefinition.description) {
+          definitionsMap[propKey] = {
+            title: propDefinition.title,
+            description: propDefinition.description,
+          };
+        }
+      }
+      processProperties(definition.properties, definitionsMap);
+    }
+  }
+  return definitionsMap;
+}
+
+export const hover = () => {
+  vscode.languages.registerHoverProvider(
+    { language: "yaml", pattern: "**/dvg.yml" },
+    {
+      provideHover(document, position) {
+        const word: string = document.getText(document.getWordRangeAtPosition(position));
+
+        /* const hoverTexts: HoverTexts = {
+          hello: "Begrüßung",
+          bye: "Abschied",
+        };
+          if (hoverTexts.hasOwnProperty(word)) {
+          return new vscode.Hover(hoverTexts[word]);
+        }
+
+        return null;
+        */
+
+        const definitionsMap: DefinitionsMap = processProperties(hoverData.$defs);
+        console.log("definitionsMap", definitionsMap);
+
+        if (definitionsMap.hasOwnProperty(word)) {
+          const hoverText = `${definitionsMap[word].title}: ${definitionsMap[word].description}`;
+          return new vscode.Hover(hoverText);
+        }
+
+        return null;
+      },
+    }
+  );
+};
--- src/extension/index.ts
+++ src/extension/index.ts
@@ -2,6 +2,7 @@ import { commands, window, ExtensionContext } from "vscode";
 import { AutoCreatePanel } from "./panels/AutoCreatePanel";
 import { SourcesProvider } from "./trees/SourcesProvider";
 import { EntitiesProvider } from "./trees/EntitiesProvider";
+import { hover } from "./LanguageFeatures/Hovering";
 
 export function activate(context: ExtensionContext) {
   const showAutoCreate = commands.registerCommand("ldproxy-editor.showAutoCreate", () => {
@@ -17,6 +18,8 @@ export function activate(context: ExtensionContext) {
     "ldproxy-editor.entityTree",
     new EntitiesProvider()
   );
+
+  const hoverFunction = hover();
 
   // Add command to the extension context
   context.subscriptions.push(showAutoCreate, storeTree, entityTree);
