148406a459c9a11e81e1d51fb9c47ace52ae03ac
PZahnen <124141114+PZahnen@users.noreply.github.com>
2023-11-22 16:35:47 +0100

    Hovering: taking references into account (recursively)

--- src/extension/LanguageFeatures/Hovering.ts
+++ src/extension/LanguageFeatures/Hovering.ts
@@ -1,12 +1,6 @@
 import * as vscode from "vscode";
 import { hoverData } from "./providers";
-import * as yaml from "js-yaml";
 import { defineDefs } from "./GetProviders";
-
-let providerType = "";
-let featureProviderType = "";
-let providerSubType = "";
-let type = "";
 
 interface LooseDefinition {
   title?: string;
@@ -23,6 +17,7 @@ function processProperties(
   definitions: Record<string, LooseDefinition>,
   definitionsMap: DefinitionsMap = {}
 ) {
+  console.log("def", defs);
   if (defs !== "") {
     const definition = definitions[defs];
     if (definition && definition.properties) {
@@ -32,6 +27,7 @@ function processProperties(
           definitionsMap[propKey] = {
             title: propDefinition.title,
             description: propDefinition.description,
+            ref: propDefinition.$ref,
           };
         }
       }
@@ -41,21 +37,38 @@ function processProperties(
   return definitionsMap;
 }
 
+/* 
+              auch noch otherProperties.ref rein*/
+function findObjectsWithRef(definitionsMap: DefinitionsMap): string[] {
+  let lastPartValueArray: string[] | undefined = [];
+  for (const key in definitionsMap) {
+    const obj = definitionsMap[key];
+    if (typeof obj === "object" && obj["ref"] !== undefined) {
+      const value = obj.ref;
+      if (value) {
+        const lastSlashIndex = value.lastIndexOf("/");
+        const lastPartValue: string = value.substring(lastSlashIndex + 1);
+        lastPartValueArray.push(lastPartValue);
+        console.log("valueeeeeeeeee", lastPartValue);
+
+        const nestedDefinitionsMap = processProperties(lastPartValue, hoverData.$defs);
+        lastPartValueArray = lastPartValueArray.concat(findObjectsWithRef(nestedDefinitionsMap));
+      }
+    }
+  }
+  return lastPartValueArray;
+}
+
 export const hover = () => {
   vscode.languages.registerHoverProvider(
     { language: "yaml", pattern: "**/dvg.yml" },
     {
       provideHover(document, position) {
-        const config = yaml.load(document.getText()) as LooseDefinition;
-        providerType = config["providerType"];
-        featureProviderType = config["featureProviderType"];
-        providerSubType = config["providerSubType"]; // Einfach moderne Schreibweise von featureProviderType
-        type = config["type"];
-
         const word: string = document.getText(document.getWordRangeAtPosition(position));
         const specifiedDefs = defineDefs(document)[0];
         const otherSpecifiedDefs = defineDefs(document)[1];
         let definitionsMap: DefinitionsMap = {};
+        let allRefs: string[] | undefined = [];
 
         console.log("specifiedDefs, otherSpecifiedDefs", specifiedDefs, otherSpecifiedDefs);
         if (
@@ -71,9 +84,18 @@ export const hover = () => {
             processProperties(specifiedDefs, hoverData.$defs)
           );
         }
+        if (definitionsMap && Object.keys(definitionsMap).length > 0) {
+          allRefs = findObjectsWithRef(definitionsMap);
+        }
 
-        console.log("definitionsMap", definitionsMap);
+        if (allRefs && allRefs.length > 0) {
+          allRefs.map((ref) => {
+            definitionsMap = Object.assign(definitionsMap, processProperties(ref, hoverData.$defs));
+          });
+        }
+
         if (definitionsMap.hasOwnProperty(word) && definitionsMap[word].description !== "") {
+          console.log("definitionsMap", definitionsMap);
           const hoverText = `${definitionsMap[word].title}: ${definitionsMap[word].description}`;
           return new vscode.Hover(hoverText);
         }
