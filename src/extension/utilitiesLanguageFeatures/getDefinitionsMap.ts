1853f7bd41ab4278630fdaa4c132094220fd3691
Andreas Zahnen <zahnen@interactive-instruments.de>
2023-12-23 16:10:38 +0100

    consolidate schema retrieval

--- src/extension/utilitiesLanguageFeatures/getDefinitionsMap.ts
+++ src/extension/utilitiesLanguageFeatures/getDefinitionsMap.ts
@@ -1,33 +1,20 @@
 import { processProperties, findObjectsWithRef } from "../utilitiesLanguageFeatures/GetProviders";
-import { getCurrentFilePath, servicesOrProviders } from "./servicesOrProviders";
-import { services } from "../utilitiesLanguageFeatures/services";
-import { hoverData } from "./providers";
+import { getSchemaDefs, DefinitionsMap } from "./schema";
 
-interface DefinitionsMap {
-  [key: string]: LooseDefinition;
-}
+export function getDefintionsMap(
+  specifiedDefs: { ref: string; finalPath: string }[]
+): DefinitionsMap {
+  const schemaDefs = getSchemaDefs();
 
-interface LooseDefinition {
-  title?: string;
-  description?: string;
-  [key: string]: any;
-}
-
-export function getDefintionsMap(specifiedDefs: { ref: string; finalPath: string }[]) {
-  let currentFilePath = getCurrentFilePath();
-  let serviceOrProvider: string | undefined;
-  if (currentFilePath) {
-    serviceOrProvider = servicesOrProviders(currentFilePath);
+  if (!schemaDefs) {
+    return {};
   }
+
   let definitionsMap: DefinitionsMap = {};
 
   let allRefs: string[] | undefined = [];
-  specifiedDefs.map((def) => {
-    if (serviceOrProvider && serviceOrProvider === "services") {
-      definitionsMap = processProperties(def.ref, services.$defs, definitionsMap);
-    } else if (serviceOrProvider && serviceOrProvider === "providers") {
-      definitionsMap = processProperties(def.ref, hoverData.$defs, definitionsMap);
-    }
+  specifiedDefs.forEach((def) => {
+    definitionsMap = processProperties(def.ref, schemaDefs, definitionsMap);
   });
   console.log("111", specifiedDefs);
   if (definitionsMap && Object.keys(definitionsMap).length > 0) {
@@ -37,17 +24,10 @@ export function getDefintionsMap(specifiedDefs: { ref: string; finalPath: string
 
   if (allRefs && allRefs.length > 0) {
     allRefs.map((ref) => {
-      if (serviceOrProvider && serviceOrProvider === "services") {
-        definitionsMap = {
-          ...definitionsMap,
-          ...processProperties(ref, services.$defs, definitionsMap),
-        };
-      } else if (serviceOrProvider && serviceOrProvider === "providers") {
-        definitionsMap = {
-          ...definitionsMap,
-          ...processProperties(ref, hoverData.$defs, definitionsMap),
-        };
-      }
+      definitionsMap = {
+        ...definitionsMap,
+        ...processProperties(ref, schemaDefs, definitionsMap),
+      };
     });
   }
   return definitionsMap;
