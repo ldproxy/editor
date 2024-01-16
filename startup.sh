e1f72cfbbc47f942a854a83ff43cd818eb110175
Andreas Zahnen <zahnen@interactive-instruments.de>
2024-01-16 12:24:43 +0100

    support git data

--- startup.sh
+++ startup.sh
@@ -4,3 +4,10 @@ set -eu
 code-server --install-extension /ldproxy-editor.vsix
 
 exec dumb-init /usr/bin/xtracfg listen 8081 --debug --verbose &
+
+if [ -n "${GIT_DATA}" ]; then
+  echo "Cloning ${GIT_DATA} to /data"
+  rm -rf /data/*
+  git clone "${GIT_DATA}" --branch main --single-branch /data
+#  git reset -C /data --hard HEAD
+fi
