dea1d3eb23a5f10d8f113daa8ba6759fbbce4ff5
Andreas Zahnen <zahnen@interactive-instruments.de>
2024-01-16 12:37:04 +0100

    support git data

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ exec dumb-init /usr/bin/xtracfg listen 8081 --debug --verbose &
 
 if [ -n "${GIT_DATA}" ]; then
   echo "Cloning ${GIT_DATA} to /data"
-  rm -rf /data/*
-  git clone "${GIT_DATA}" --branch main --single-branch /data
+  sudo rm -rf /data/*
+  sudo git clone "${GIT_DATA}" --branch main --single-branch /data
 #  git reset -C /data --hard HEAD
 fi
