b325a3fdcf7142cad112f351cd66238bedbb6683
Andreas Zahnen <zahnen@interactive-instruments.de>
2024-01-16 13:10:14 +0100

    password from env

--- startup.sh
+++ startup.sh
@@ -5,6 +5,10 @@ code-server --install-extension /ldproxy-editor.vsix
 
 exec dumb-init /usr/bin/xtracfg listen 8081 --debug --verbose &
 
+if [ -n "${PASSWORD}" ]; then 
+  sudo export AUTH="password"
+fi
+
 if [ -n "${GIT_DATA}" ]; then
   echo "Cloning ${GIT_DATA} to /data"
   sudo rm -rf /data/*
