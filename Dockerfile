5fb751c435c9154e803e18f13d55ec1672d93d2a
Andreas Zahnen <zahnen@interactive-instruments.de>
2024-01-25 10:10:21 +0100

    fix password from env and permissions

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,9 @@ COPY --chmod=0644 --from=extension /src/dist/ldproxy-editor.vsix /
 COPY --chmod=0755 --from=extension /src/startup.sh /entrypoint.d/
 COPY --chmod=0755 --from=xtracfg /xtracfg /usr/bin/
 
+ENV HOME=/home/coder
 VOLUME /home/coder
 VOLUME /data
+USER root
 
 ENTRYPOINT ["/usr/bin/entrypoint.sh", "--ignore-last-opened", "--bind-addr", "0.0.0.0:80", "--welcome-text", "\"Hello\"", "--app-name", "\"ldproxy-editor\"", "--disable-telemetry", "--disable-update-check", "--disable-workspace-trust", "--disable-getting-started-override", "/data"]
--- startup.sh
+++ startup.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-set -eu
+set -e
 
-code-server --install-extension /ldproxy-editor.vsix
-
-exec dumb-init /usr/bin/xtracfg listen 8081 --debug --verbose &
-
-if [ -n "${PASSWORD}" ]; then 
-  export AUTH="password"
-else
-  export AUTH="none"
+if [ -z "${PASSWORD}" ]; then 
+  mkdir -p /home/coder/.config/code-server
+  echo "auth: none" > /home/coder/.config/code-server/config.yaml
+  echo "cert: false" >> /home/coder/.config/code-server/config.yaml
 fi
 
 if [ -n "${GIT_DATA}" ]; then
@@ -17,3 +13,9 @@ if [ -n "${GIT_DATA}" ]; then
   sudo chown coder. /data
   git clone "${GIT_DATA}" --branch "${GIT_DATA_BRANCH:-master}" --single-branch /data
 fi
+
+code-server --install-extension /ldproxy-editor.vsix
+
+exec dumb-init /usr/bin/xtracfg listen 8081 --debug --verbose &
+
+
