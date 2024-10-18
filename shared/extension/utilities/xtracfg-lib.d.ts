d1781e7cda77ccccdc6bc4bf06a95dcff252287e
PZahnen <pascal.zahnen@gmx.de>
2024-10-18 13:03:51 +0200

    native extension running

--- shared/extension/utilities/xtracfg-lib.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "xtracfg" {
-  export function xtracfg(command: string): any;
-  export function subscribe(callback: (progress: string) => void): void;
-}
---
+++ shared/extension/utilities/xtracfg.d.ts
@@ -0,0 +1,28 @@
+declare module "xtracfg" {
+  export function xtracfg(command: string): any;
+  export function subscribe(callback: (progress: string) => void): void;
+}
+// Fügen Sie die Deklaration für die connect-Funktion hinzu
+export function connect(transport: TransportCreator, options?: TransportOptions): Xtracfg;
+
+// Fügen Sie die Typen hinzu, die in Ihrem Code verwendet werden
+export type Xtracfg = {
+  send: (request: any) => Promise<void>;
+  listen: (
+    successHandler: (response: any) => void,
+    errorHandler: (error: Error) => void
+  ) => Promise<void>;
+};
+
+export type TransportCreator = (options: TransportOptions) => TransportConnector;
+
+export type TransportConnector = () => Promise<Transport>;
+
+export type TransportOptions = {
+  debug?: boolean;
+};
+
+export type Transport = {
+  send: (request: any) => Promise<void>;
+  listen: (handler: (response: any) => void) => Promise<void>;
+};
