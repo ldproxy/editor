dc6f5d9ed5fe97859e0b8885445d17afe5d56f7a
Andreas Zahnen <zahnen@interactive-instruments.de>
2024-11-19 16:16:10 +0100

    cleanup and improve build

--- native/webpack.config.cjs
+++ native/webpack.config.cjs
@@ -13,78 +13,81 @@ const path = require("path");
 const webpack = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
 
-/** @type WebpackConfig */
-const nativeExtensionConfig = {
-  mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-  target: "node",
-  node: {
-    __dirname: false, // required for native node addons
-  },
-  //stats: "verbose",
-  entry: {
-    extension: "./index.ts", // source of the web extension main file
-    //'test/suite/index': './src/web/test/suite/index.ts', // source of the web extension test runner
-  },
-  output: {
-    filename: "[name].js",
-    path: path.join(__dirname, "./dist"),
-    libraryTarget: "commonjs",
-    devtoolModuleFilenameTemplate: "../[resource-path]",
-  },
-  resolve: {
-    mainFields: ["module", "main"], // look for `browser` entry point in imported node modules
-    extensions: [".ts", ".js"], // support ts-files and js-files
-    alias: {
-      // provides alternate implementation for node module and source files
+/** @type (env:any,argv:any) => WebpackConfig */
+const nativeExtensionConfig = (env, argv) => {
+  const out = argv.mode === "development" ? "../dist/dev" : "../dist/native";
+
+  return {
+    target: "node",
+    node: {
+      __dirname: false, // required for native node addons
     },
-    fallback: {
-      // Webpack 5 no longer polyfills Node.js core modules automatically.
-      // see https://webpack.js.org/configuration/resolve/#resolvefallback
-      // for the list of Node.js core module polyfills.
-      assert: require.resolve("assert"),
-      path: require.resolve("path-browserify"),
+    //stats: "verbose",
+    entry: {
+      extension: "./index.ts", // source of the web extension main file
+      //'test/suite/index': './src/web/test/suite/index.ts', // source of the web extension test runner
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader",
-          },
-        ],
+    output: {
+      filename: `[name].js`,
+      path: path.join(__dirname, out),
+      libraryTarget: "commonjs",
+      devtoolModuleFilenameTemplate: "../[resource-path]",
+    },
+    resolve: {
+      mainFields: ["module", "main"], // look for `browser` entry point in imported node modules
+      extensions: [".ts", ".js"], // support ts-files and js-files
+      alias: {
+        // provides alternate implementation for node module and source files
       },
-    ],
-  },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1, // disable chunks by default since web extensions must be a single bundle
-    }),
-    new webpack.ProvidePlugin({
-      process: "process", // provide a shim for the global `process` variable
-    }),
-    new CopyPlugin({
-      patterns: [
+      fallback: {
+        // Webpack 5 no longer polyfills Node.js core modules automatically.
+        // see https://webpack.js.org/configuration/resolve/#resolvefallback
+        // for the list of Node.js core module polyfills.
+        assert: require.resolve("assert"),
+        path: require.resolve("path-browserify"),
+      },
+    },
+    module: {
+      rules: [
         {
-          context: "./node_modules/@xtracfg/native",
-          from: "prebuilds/**/*.node",
-          to: "./",
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "ts-loader",
+            },
+          ],
         },
       ],
-    }),
-  ],
-  externals: {
-    vscode: "commonjs vscode", // ignored because it doesn't exist
-  },
-  performance: {
-    hints: false,
-  },
-  devtool: "nosources-source-map", // create a source map that points to the original source file
-  infrastructureLogging: {
-    level: "log", // enables logging required for problem matchers
-  },
+    },
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1, // disable chunks by default since web extensions must be a single bundle
+      }),
+      new webpack.ProvidePlugin({
+        process: "process", // provide a shim for the global `process` variable
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            context: "../node_modules/@xtracfg/native",
+            from: "prebuilds/**/*.node",
+            to: "./",
+          },
+        ],
+      }),
+    ],
+    externals: {
+      vscode: "commonjs vscode", // ignored because it doesn't exist
+    },
+    performance: {
+      hints: false,
+    },
+    devtool: "nosources-source-map", // create a source map that points to the original source file
+    infrastructureLogging: {
+      level: "log", // enables logging required for problem matchers
+    },
+  };
 };
 
-module.exports = [nativeExtensionConfig];
+module.exports = nativeExtensionConfig;
