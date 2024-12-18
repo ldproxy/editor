a2a88b85db041b86d478738b3e60ca658711ed25
p.zahnen <pascal.zahnen@gmx.de>
2024-12-18 16:46:27 +0100

    refactored completions provider 2

--- src/extension/utilities/completions.ts
+++ src/extension/utilities/completions.ts
@@ -1,3 +1,5 @@
+import * as vscode from "vscode";
+
 interface YamlKey {
   path: string;
   index: number;
@@ -42,4 +44,74 @@ export function getPathAtCursor(allYamlKeys: YamlKey[], line: number, column: nu
   }
 
   return column > 0 ? getPathAtCursorString(indexToUse, column, allYamlKeys) : "";
+}
+
+export function shouldFilterExistingCharacters(
+  textBeforeCursor: string,
+  finalValue: string
+): boolean {
+  if (textBeforeCursor.trim() !== "") {
+    if (textBeforeCursor.trim().includes("-")) {
+      const textWithoutHyphen = textBeforeCursor.trim().replace("-", "");
+      return finalValue.startsWith(textWithoutHyphen.trim());
+    } else {
+      return finalValue.startsWith(textBeforeCursor.trim());
+    }
+  }
+  return true;
+}
+
+export function createCompletionItem(
+  finalValue: string,
+  obj2: any,
+  enumArray: any[],
+  DEV: boolean
+): vscode.CompletionItem {
+  const completion = new vscode.CompletionItem(finalValue);
+  if (obj2.type && obj2.type === "object") {
+    completion.insertText = `${finalValue}: \n  `;
+  } else if (obj2.type && obj2.type === "array") {
+    completion.insertText = `${finalValue}: \n  - `;
+  } else {
+    completion.insertText = `${finalValue}: `;
+  }
+  completion.kind = vscode.CompletionItemKind.Method;
+  if (
+    enumArray.length > 0 &&
+    enumArray.some((enumItem) => {
+      return (
+        obj2.title === enumItem.key &&
+        (obj2.groupname === enumItem.groupname ||
+          (obj2.groupname === "_TOP_LEVEL_" && enumItem.groupname === ""))
+      );
+    })
+  ) {
+    completion.detail = "Enum";
+  } else if (obj2.type === "object" && obj2.ref !== "") {
+    completion.detail = "obj";
+  } else if (obj2.type === "array" && obj2.ref !== "") {
+    completion.detail = "array";
+  }
+
+  if (obj2.description !== "") {
+    completion.documentation = new vscode.MarkdownString(obj2.description);
+  }
+
+  if (
+    completion.detail === "Enum" ||
+    completion.detail === "obj" ||
+    completion.detail === "array"
+  ) {
+    completion.command = {
+      title: "Trigger Suggest",
+      command: "editor.action.triggerSuggest",
+    };
+  }
+
+  if (DEV) {
+    console.log("completion1", finalValue);
+    console.log("insertText1", completion.insertText);
+  }
+
+  return completion;
 }
